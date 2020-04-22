023fe1fe66ef1d1a487d7e6a35cf85fed88d5206
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-04-23 01:44:26 +0530

    Update truncate-a-string.js

--- truncate-a-string.js
+++ truncate-a-string.js
@@ -6,6 +6,13 @@ function truncateString(str, num) {
       arr+="...";
       /*
       // Suppose in place of the above 2 lines, we were using 
+      >> arr=str.slice(0,num);
+      >> arr.push("...")
+      
+      Then we would be thrown a push is not a function error, as arr currently is of type string, and push/pop is a function of array.
+      So we need to convert it to array before applying push()/pop() to it.
+      We achieve this by performing the type-casting in the following manner 
+      >> arr=[str.slice(0,num)]
       
       */
       // console.log(arr)
