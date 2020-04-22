03c0fee922b80a224c2da5d1a44b4f32d3b29765
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-04-23 01:40:33 +0530

    Update truncate-a-string.js

--- truncate-a-string.js
+++ truncate-a-string.js
@@ -2,8 +2,12 @@ function truncateString(str, num) {
   if(str.length>num)
     {
       let arr=new Array();
-      arr=[str.slice(0,num)];
+      arr=str.slice(0,num);
       arr+="...";
+      /*
+      // Suppose in place of the above 2 lines, we were using 
+      
+      */
       // console.log(arr)
       return arr;
     }
